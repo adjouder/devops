c2827cf0a334b42bd2a6de57fa2f293f0be54b8f
DJOUDER <djouder@Ordinateur-portable-de-DJOUDER.local>
2024-04-17 11:54:57 +0200

    validation du lab

--- whizlabs/aws-eks-terraform/task_10132/main.tf
+++ whizlabs/aws-eks-terraform/task_10132/main.tf
@@ -7,9 +7,9 @@ provider "aws"{
 ################## Creating an EKS Cluster ##################
 resource "aws_eks_cluster" "cluster" {
   name     = "whiz"
-  role_arn = "arn:aws:iam::124787947185:role/task98_role_152500.35844158"
+  role_arn = "arn:aws:iam::685533198991:role/task98_role_152500.53589894s"
 
   vpc_config {
-    subnet_ids = ["subnet-0bc65a52b3bb700ef", "subnet-07a3b0da3cc79ea3e"]
+    subnet_ids = ["	subnet-0752de048b5275198", "subnet-094d8c91035542f2e"]
   }
 }
